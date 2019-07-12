3c27607b5d4982fa57d43d5da19d03c5a61270ba
swetanjal <swetanjal.dutta@research.iiit.ac.in>
2019-07-12 23:26:12 +0530

    Modify Modular Exponentiation.

--- ACM ICPC Codebook/Number Theory/modular exponentiation.cpp
+++ ACM ICPC Codebook/Number Theory/modular exponentiation.cpp
@@ -1,13 +1,13 @@
 // Start of template for mod exponentiation
 long long modExp(long long a, long long b, long long p)
 {
-	if(b==0){
+	if(b == 0){
 		return 1;
 	}
-	if(b==1)return a;
-	long long ttt=modExp(a,b/2,p);
-	ttt=(ttt*ttt)%p;
-	if((b%2)==0)return ttt;
-	return (ttt*a)%p;
+	if(b == 1)return (a % p);
+	long long ttt= modExp(a, b / 2, p);
+	ttt = (ttt * ttt) % p;
+	if((b % 2) == 0)return ttt;
+	return ((ttt % p) * (a % p)) % p;
 }
 // End of template for mod exponentiation
